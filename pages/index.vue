4d4ebe156925f9cab0b72d907e183791fda409db
Dhanvant Sonagara <dhanvant@closedigit.com>
2025-03-22 18:06:14 +0530

    text changed

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    <button>Click me</button>
+    <button>Click me main</button>
     <p>Branch 1</p>
   </div>
 </template>
